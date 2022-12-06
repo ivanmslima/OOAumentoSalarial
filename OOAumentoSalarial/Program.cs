776ae934b854ffa00d8bf0ff5a05155b4f465d96
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-12-06 18:21:22 -0300

    Adicionar arquivos de projeto.

---
+++ OOAumentoSalarial/Program.cs
@@ -0,0 +1,37 @@
+﻿using OOAumentoSalarial;
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentoSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+
+
+
+        }
+    }
+}
